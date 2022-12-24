90aed78904388d1fb443c51bb634feac0642f5e4
greveson.axel@gmail.com <greveson.axel@gmail.com>
2022-12-24 21:07:59 +0100

    update

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-const menu = document.querySelector(".menu");
-let isOpen = false;
-
 function toggle_menu() {
+  const menu = document.querySelector(".menu");
+  let isOpen = false; 
   if (!menu.classList.contains("is-open")) {
     isOpen = true;
     menu.classList.toggle("is-open", true);
@@ -10,5 +9,27 @@ function toggle_menu() {
     isOpen = false;
     menu.classList.toggle("is-open", false);
     console.log("Closed");
+  }
+}
+
+function activeState() {
+  let elem = event.target;
+  let prev = elem.previousElementSibling;
+  if (!elem.value) {
+    prev.classList.toggle("focus")
+  } else {
+    prev.classList.toggle("focus", true)
+  }
+}
+
+function check() {
+  const username = document.querySelector("[name='username']");
+  const password = document.querySelector("[name='password']");
+  const confPassword = document.querySelector("[name='confPassword']");
+  
+  if (password.value != confPassword.value) {
+    console.log("different passwords");
+  } else {
+    console.log("same passwords");
   }
 }
